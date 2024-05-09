8f2d804a2a6b793c5232e058bf175bea8d9006df
lakton <lakton1337@gmail.com>
2024-05-09 19:51:35 +0300

    asd

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -4,6 +4,7 @@ flag = 0
 total_tests = 0
 correct_tests = 0
 incorrect_tests = 0
+unreachable_tests = 0
 
 if __name__ == "__main__":
     with open("/home/sdn/Desktop/2/results/ping.log", "r") as test, open("/home/sdn/Desktop/2/results/connectivity_test_results", "w") as output_file:
@@ -11,7 +12,7 @@ if __name__ == "__main__":
             if re.match("(.*)ping(.*)", line):
                 output_file.write("=========================\n")
                 output_file.write(line)
-                total_tests += 0.5
+                total_tests += 1
                 
             if re.match("(.*)ws1(.*)", line) or re.match("(.*)ds1(.*)", line):
                 if re.match("(.*)h1(.*)", line) or re.match("(.*)h2(.*)", line):
@@ -27,6 +28,12 @@ if __name__ == "__main__":
                 number = work.split()
                 output_file.write("Пакетов отправлено = " + number[0] + "\n")
                 output_file.write("Пакетов принято = " + number[3] + "\n")
+                
+                if "unreachable" in line:
+                    output_file.write("ПРОВЕРЬ САМ\n")
+                    unreachable_tests += 1
+                    continue
+                
                 if flag == 1:
                     flag = 0
                     output_file.write("Процент потерь пакетов = " + number[7] + "\n")
@@ -49,13 +56,14 @@ if __name__ == "__main__":
                     output_file.write("Процент потерь пакетов = " + number[5] + "\n")
                     if number[5] == "100%":
                         output_file.write("FAIL\n")
-                        correct_tests += 1
+                        incorrect_tests += 1
                     else:
                         output_file.write("PASS\n")
-                        incorrect_tests += 1
+                        correct_tests += 1
 
-        # Запись информации о количестве тестов и количестве корректных и некорректных результатов
+        # Write summary of test results
         output_file.write("\n=========================\n")
         output_file.write("Общее количество тестов: {}\n".format(total_tests))
         output_file.write("Количество корректных результатов: {}\n".format(correct_tests))
         output_file.write("Количество некорректных результатов: {}\n".format(incorrect_tests))
+        output_file.write("Количество unreachable: {}\n".format(unreachable_tests))
