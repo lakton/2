46e4b45c4417cc80f5f18e6b85acbf4d4e5e2f6c
lakton <lakton1337@gmail.com>
2024-05-09 19:41:57 +0300

    asdas

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -1,14 +1,18 @@
 import re
 
 flag = 0
-total_tests = 0
+test_number = 0
+incorrect_results = 0
 correct_results = 0
+total_tests = 0
 
 if __name__ == "__main__":
     with open("/home/sdn/Desktop/2/results/ping.log", "r") as test, open("/home/sdn/Desktop/2/results/connectivity_test_results", "w") as output_file:
         for line in test:
             if re.match("(.*)ping(.*)", line):
-                output_file.write("=========================\n")
+                test_number += 1
+                total_tests += 1
+                output_file.write(f"====ТЕСТ №{test_number}====\n")
                 output_file.write(line)
                 
             if re.match("(.*)ws1(.*)", line) or re.match("(.*)ds1(.*)", line):
@@ -21,7 +25,6 @@ if __name__ == "__main__":
                 output_file.write(line)
                 
             if re.match("(.*)packet(.*)", line):
-                total_tests += 1
                 work = line
                 number = work.split()
                 output_file.write("Пакетов отправлено = " + number[0] + "\n")
@@ -31,24 +34,30 @@ if __name__ == "__main__":
                     output_file.write("Процент потерь пакетов = " + number[7] + "\n")
                     if number[7] == "100%":
                         output_file.write("PASS\n")
+                        correct_results += 1
                     else:
                         output_file.write("FAIL\n")
+                        incorrect_results += 1
                 elif flag == 2:
                     flag = 0
                     output_file.write("Процент потерь пакетов = " + number[5] + "\n")
                     if number[5] == "100%":
                         output_file.write("PASS\n")
+                        correct_results += 1
                     else:
                         output_file.write("FAIL\n")
+                        incorrect_results += 1
                 else:
                     output_file.write("Процент потерь пакетов = " + number[5] + "\n")
                     if number[5] == "100%":
                         output_file.write("FAIL\n")
+                        correct_results += 1
                     else:
                         output_file.write("PASS\n")
-                    correct_results += 1
+                        incorrect_results += 1
 
-    # Перемещаем запись количества тестов и корректных результатов за пределы блока with open(...):
+    # Запись общего количества тестов и количества корректных тестов
     with open("/home/sdn/Desktop/2/results/connectivity_test_results", "a") as output_file:
         output_file.write(f"\nОбщее количество тестов: {total_tests}\n")
-        output_file.write(f"Количество корректных результатов: {correct_results}\n")
+        output_file.write(f"Количество корректных тестов: {correct_results}\n")
+        output_file.write(f"Количество НЕкорректных тестов: {incorrect_results}\n")
