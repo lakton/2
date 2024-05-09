51ca64a67ee2c6a698ffbd3befef66ed7d73965e
lakton <lakton1337@gmail.com>
2024-05-09 16:31:28 +0300

    фыв

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -3,37 +3,32 @@
 import re
 
 if __name__ == "__main__":
-    test = open("/home/sdn/Desktop/2/results/ping.log", "r")
+    flag = False
     results = []
-    test_info = ""
-    for line in test:
-        if re.match("(.*)ping(.*)", line):
-            if test_info:
-                results.append(test_info)
-            test_info = line.strip()
-        elif re.match("=========================", line):
-            results.append(line.strip())
-        elif "DUP!" in line:
-            continue
-        else:
-            test_info += "\n" + line.strip()
 
-        if re.match(".*connect.*", line):
-            if re.match(".*packet loss.*", line):
-                packet_info = line.split()
-                transmitted = packet_info[0]
-                received = packet_info[3]
-                loss_percent = packet_info[7] if packet_info[7] != "100%" else "+100"  # Handle the 100% loss case
-                result = "transmitted = {}\nreceived = {}\npacket loss percent = {}\n".format(transmitted, received, loss_percent)
-                if loss_percent == "+100":
-                    result += "PASS"
+    with open("/home/sdn/Desktop/2/results/ping.log", "r") as test:
+        for line in test:
+            if re.match(".*ping.*", line):
+                results.append("=========================")
+                results.append(line.strip())  # Добавление строки пинга
+
+            elif re.match(".*connect:.*", line):
+                results.append(line.strip())  # Добавление строки ошибки соединения
+
+            elif re.match(".*ping statistics.*", line):
+                results.append(line.strip())  # Добавление строки статистики пинга
+                stats = next(test)  # Чтение следующей строки для получения статистики
+                transmitted = int(re.search(r"transmitted = (\d+)", stats).group(1))
+                received = int(re.search(r"received = (\d+)", stats).group(1))
+                loss = re.search(r"packet loss percent = ([\d+%-]+)", stats).group(1)
+                if "100%" in loss:
+                    result = "FAIL"
                 else:
-                    result += "FAIL"
+                    result = "PASS"
+                results.append("transmitted = " + str(transmitted))
+                results.append("received = " + str(received))
+                results.append("packet loss percent = " + loss)
                 results.append(result)
-
-    # Добавляем последний блок теста
-    if test_info:
-        results.append(test_info)
 
     with open("/home/sdn/Desktop/2/results/connectivity_test_results", "w") as output_file:
         for result in results:
