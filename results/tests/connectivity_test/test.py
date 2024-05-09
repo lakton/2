69232d8e53606bf126dea5b7187fa533cc5abc26
lakton <lakton1337@gmail.com>
2024-05-09 19:49:13 +0300

    asdasda

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
             if re.match("(.*)ping(.*)", line):
                 output_file.write("=========================\n")
                 output_file.write(line)
-                total_tests += 1/2
+                total_tests += 0.5
                 
             if re.match("(.*)ws1(.*)", line) or re.match("(.*)ds1(.*)", line):
                 if re.match("(.*)h1(.*)", line) or re.match("(.*)h2(.*)", line):
@@ -49,10 +49,10 @@ if __name__ == "__main__":
                     output_file.write("Процент потерь пакетов = " + number[5] + "\n")
                     if number[5] == "100%":
                         output_file.write("FAIL\n")
-                        incorrect_tests += 1
+                        correct_tests += 1
                     else:
                         output_file.write("PASS\n")
-                        correct_tests += 1
+                        incorrect_tests += 1
 
         # Запись информации о количестве тестов и количестве корректных и некорректных результатов
         output_file.write("\n=========================\n")
