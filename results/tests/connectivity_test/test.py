cad1618bbd5f19b21019190c3567b9a783b48ba0
lakton <lakton1337@gmail.com>
2024-05-09 19:13:45 +0300

    ФЫВ

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -3,79 +3,58 @@
 import re
 
 if __name__ == "__main__":
-    # Словарь для хранения флагов для каждого узла
-    node_flags = {
-        "ws1": 0, "ds1": 0, "h1": 0, "h2": 0, "h3": 0, "h4": 0, "dnslb": 0, "wwwlb": 0, "napt": 0
-    }
-
-    # Открываем файл с результатами ping-тестов для чтения
+    node_flags = {"ws1": 0, "ds1": 0, "h1": 0, "h2": 0, "h3": 0, "h4": 0, "dnslb": 0, "wwwlb": 0, "napt": 0}
     test = open("/home/sdn/Desktop/2/results/ping.log", "r")
     results = []
-
-    # Цикл по строкам в файле
+    ping_data = []
     for line in test:
-        # Проверяем, соответствует ли строка формату ping
         if re.match("(.*)ping(.*)", line):
-            # Если есть данные о предыдущем ping-тесте, добавляем их к результатам
-            if results:
-                results.append("=========================")
+            if ping_data:
                 results.extend(ping_data)
                 ping_data = []
-
-            results.append(line.strip())  # Добавляем строку с заголовком ping-теста
+            results.append("=========================")
+            results.append(line.strip())
         elif re.match(".*connect.*", line):
-            results.append(line.strip())  # Добавляем строки о соединении
+            results.append(line.strip())
         elif re.match("(.*)packet(.*)", line):
-            # Обрабатываем строку с данными о пакетах
             work = line
             number = work.split()
+            ping_data.append("transmitted = " + number[0])
+            ping_data.append("received = " + number[3])
             transmitted = int(number[0])
             received = int(number[3])
             packet_loss_percent = (1 - received / transmitted) * 100
-
-            # Проверяем условия для каждой пары узлов
-            if "h3" in line or "h4" in line:
-                if "ws1" in line or "ds1" in line:
-                    if packet_loss_percent == 100:
-                        results.append("PASS")
-                    else:
-                        results.append("FAIL")
-                else:
-                    if packet_loss_percent != 100:
-                        results.append("PASS")
-                    else:
-                        results.append("FAIL")
-            elif "h1" in line or "h2" in line:
-                if "ws1" in line or "ds1" in line:
-                    if packet_loss_percent == 100:
-                        results.append("PASS")
-                    else:
-                        results.append("FAIL")
-                else:
-                    if packet_loss_percent != 100:
-                        results.append("PASS")
-                    else:
-                        results.append("FAIL")
+            ping_data.append("packet loss percent = " + f"{packet_loss_percent:.2f}%")
+            if any(node_flags.values()):
+                for node, flag in node_flags.items():
+                    if flag == 1:
+                        flag = 0
+                        if packet_loss_percent <= 99 and node not in ["ws1", "ds1"]:
+                            ping_data.append("PASS")
+                        else:
+                            ping_data.append("FAIL")
+                    elif flag == 2:
+                        flag = 0
+                        if packet_loss_percent <= 99 and node not in ["ws1", "ds1"]:
+                            ping_data.append("PASS")
+                        else:
+                            ping_data.append("FAIL")
+                    elif flag == 3:
+                        flag = 0
+                        if packet_loss_percent <= 99 and node not in ["ws1", "ds1"]:
+                            ping_data.append("PASS")
+                        else:
+                            ping_data.append("FAIL")
             else:
-                if packet_loss_percent != 100:
-                    results.append("PASS")
+                if packet_loss_percent <= 99 and "ws1" not in line and "ds1" not in line:
+                    ping_data.append("PASS")
                 else:
-                    results.append("FAIL")
-
-            # Форматируем данные о пакетах и добавляем их к результатам
-            ping_data = [
-                "--- " + number[1] + " ping statistics ---",
-                "transmitted = " + number[0],
-                "received = " + number[3],
-                "packet loss percent = " + f"{packet_loss_percent:.2f}%"
-            ]
-
-    # Добавляем данные о последнем ping-тесте к результатам
+                    ping_data.append("FAIL")
+                    if packet_loss_percent <= 99 and ("dnslb" in line or "wwwlb" in line or "napt" in line):
+                        ping_data.pop()  # Если пакет потерян, но не на всех узлах, применяем FAIL только в этом случае
     if ping_data:
-        results.append("=========================")
         results.extend(ping_data)
 
-    # Записываем результаты в файл
     with open("/home/sdn/Desktop/2/results/connectivity_test_results", "w") as output_file:
         for result in results:
             output_file.write(result + '\n')
