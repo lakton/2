eff35ad0dd3e91a159a871a33329d60113db323d
lakton <lakton1337@gmail.com>
2024-05-09 16:15:07 +0300

    фыв

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -7,40 +7,40 @@ if __name__ == "__main__":
     test = open("/home/sdn/Desktop/2/results/ping.log", "r")
     results = []
     for line in test:
-        print("Processing line:", line.strip())  # Добавлено отладочное сообщение
-        if re.match("(.*)ping(.*)", line):
-            print("Matched 'ping' pattern")  # Добавлено отладочное сообщение
+        print("Обрабатывается строка:", line.strip())  # Добавлено отладочное сообщение
+        if re.match(".*ping.*", line):
+            print("Совпадение с шаблоном 'ping'")  # Добавлено отладочное сообщение
             results.append("=========================")
         results.append(line.strip())  # Удаление символа новой строки для избежания лишних переносов строк
-        if re.match("(.*)ws1(.*)", line) or re.match("(.*)ds1(.*)", line):
-            if re.match("(.*)h1(.*)", line) or re.match("(.*)h2(.*)", line):
+        if re.match(".*ws1.*", line) or re.match(".*ds1.*", line):
+            if re.match(".*h1.*", line) or re.match(".*h2.*", line):
                 flag = 1
             else:
                 flag = 2
 
         if re.match(".*connect.*", line):
             results.append(line.strip())
-            if re.match("(.*)packet(.*)", line):
+            if re.match(".*packet.*", line):
                 work = line
                 number = work.split()
-                results.append("передано = " + number[0])
-                results.append("получено = " + number[3])
+                results.append("Передано = " + number[0])
+                results.append("Получено = " + number[3])
                 if flag == 1:
                     flag = 0
-                    results.append("процент пакет лосса = " + number[7])
+                    results.append("Процент потерь = " + number[7])
                     if number[7] == "100%":
                         results.append("PASS")
                     else:
                         results.append("FAIL")
                 elif flag == 2:
                     flag = 0
-                    results.append("процент пакет лосса = " + number[5])
+                    results.append("Процент потерь = " + number[5])
                     if number[5] == "100%":
                         results.append("PASS")
                     else:
                         results.append("FAIL")
                 else:
-                    results.append("процент пакет лосса = " + number[5])
+                    results.append("Процент потерь = " + number[5])
                     if number[5] == "100%":
                         results.append("FAIL")
                     else:
