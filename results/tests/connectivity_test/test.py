3f01157fd70174eac4987601ed11602f686123aa
lakton <lakton1337@gmail.com>
2024-05-09 17:01:29 +0300

    фывыфвыфвфыв

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -25,27 +25,27 @@ if __name__ == "__main__":
                 for node, flag in node_flags.items():
                     if flag == 1:
                         flag = 0
-                        ping_data.append("packet loss percent = " + number[7].replace('+', ''))  # Убираем '+' из процента потерь
-                        if float(number[7].replace('%', '')) == 0 and node != "ws1" and node != "ds1":
+                        ping_data.append("packet loss percent = " + number[7])
+                        if number[7] == "100%" and node != "ws1" and node != "ds1":
                             ping_data.append("PASS")
                         else:
                             ping_data.append("FAIL")
                     elif flag == 2:
                         flag = 0
                         ping_data.append("packet loss percent = " + number[5])
-                        if float(number[5].replace('%', '')) == 0 and node != "ws1" and node != "ds1":
+                        if number[5] == "100%" and node != "ws1" and node != "ds1":
                             ping_data.append("PASS")
                         else:
                             ping_data.append("FAIL")
                     else:
                         ping_data.append("packet loss percent = " + number[5])
-                        if float(number[5].replace('%', '')) == 0 and node != "ws1" and node != "ds1":
+                        if number[5] == "100%" and node != "ws1" and node != "ds1":
                             ping_data.append("PASS")
                         else:
                             ping_data.append("FAIL")
             else:
                 ping_data.append("packet loss percent = " + number[5])
-                if float(number[5].replace('%', '')) == 0 and "ws1" not in line and "ds1" not in line:  # Учитываем случаи с ws1 и ds1
+                if number[5] == "100%" and "ws1" not in line and "ds1" not in line:  # Учитываем случаи с ws1 и ds1
                     ping_data.append("PASS")
                 else:
                     ping_data.append("FAIL")
