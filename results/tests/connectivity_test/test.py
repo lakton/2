b5171e3e2fb4141dc34f9cbd943a05ed04075764
lakton <lakton1337@gmail.com>
2024-05-09 19:38:01 +0300

    фыв

--- results/tests/connectivity_test/test.py
+++ results/tests/connectivity_test/test.py
@@ -1,6 +1,8 @@
 import re
 
 flag = 0
+total_tests = 0
+correct_results = 0
 
 if __name__ == "__main__":
     with open("/home/sdn/Desktop/2/results/ping.log", "r") as test, open("/home/sdn/Desktop/2/results/connectivity_test_results", "w") as output_file:
@@ -19,27 +21,32 @@ if __name__ == "__main__":
                 output_file.write(line)
                 
             if re.match("(.*)packet(.*)", line):
+                total_tests += 1
                 work = line
                 number = work.split()
-                output_file.write("получено = " + number[0] + "\n")
-                output_file.write("принято = " + number[3] + "\n")
+                output_file.write("Пакетов отправлено = " + number[0] + "\n")
+                output_file.write("Пакетов принято = " + number[3] + "\n")
                 if flag == 1:
                     flag = 0
-                    output_file.write("packet loss percent = " + number[7] + "\n")
+                    output_file.write("Процент потерь пакетов = " + number[7] + "\n")
                     if number[7] == "100%":
                         output_file.write("PASS\n")
                     else:
                         output_file.write("FAIL\n")
                 elif flag == 2:
                     flag = 0
-                    output_file.write("packet loss percent = " + number[5] + "\n")
+                    output_file.write("Процент потерь пакетов = " + number[5] + "\n")
                     if number[5] == "100%":
                         output_file.write("PASS\n")
                     else:
                         output_file.write("FAIL\n")
                 else:
-                    output_file.write("packet loss percent = " + number[5] + "\n")
+                    output_file.write("Процент потерь пакетов = " + number[5] + "\n")
                     if number[5] == "100%":
                         output_file.write("FAIL\n")
                     else:
                         output_file.write("PASS\n")
+                    correct_results += 1
+
+    output_file.write(f"\nОбщее количество тестов: {total_tests}\n")
+    output_file.write(f"Количество корректных результатов: {correct_results}\n")
