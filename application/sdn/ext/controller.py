1a78870200b7a2554d646b294535892b75d1ee06
lakton <lakton1337@gmail.com>
2024-05-10 13:12:09 +0300

    asd

--- application/sdn/ext/controller.py
+++ application/sdn/ext/controller.py
@@ -213,7 +213,8 @@ class LearningFirewall1(LearningFirewall):
         # Правила для доступа к h1 и h2 (в зоне PbZ)
         self.AddRule('00-00-00-00-00-02', EthAddr('00:00:00:00:00:04'), 0, True)
         self.AddRule('00-00-00-00-00-02', EthAddr('00:00:00:00:00:05'), 0, True) 
-        self.AddRule('00-00-00-00-00-02', EthAddr('8a:11:96:8b:b0:e5'), 8, True) 
+        self.AddRule('00-00-00-00-00-02', EthAddr('8a:11:96:8b:b0:e5'), 8, True)
+        self.AddRule('00-00-00-00-00-02', EthAddr('4a:1c:a8:0c:07:20'), 8, True)
 
         # Запрет ICMP к h3 и h4
         self.AddRule('00-00-00-00-00-02', EthAddr('00:00:00:00:00:06'), 8, False) 
@@ -251,7 +252,8 @@ class LearningFirewall2(LearningFirewall):
         # Правила для доступа к h1 и h2 (в зоне PbZ)
         self.AddRule('00-00-00-00-00-02', EthAddr('00:00:00:00:00:04'), 8, True)
         self.AddRule('00-00-00-00-00-02', EthAddr('00:00:00:00:00:05'), 8, True)
-        self.AddRule('00-00-00-00-00-02', EthAddr('4a:1c:a8:0c:07:20'), 0, True) 
+        self.AddRule('00-00-00-00-00-02', EthAddr('4a:1c:a8:0c:07:20'), 0, True)
+        self.AddRule('00-00-00-00-00-02', EthAddr('8a:11:96:8b:b0:e5'), 0, True) 
         
         # Правила для доступа к h3 и h4 (в зоне PrZ)
         self.AddRule('00-00-00-00-00-09', EthAddr('00:00:00:00:00:06'), 0, True) 
