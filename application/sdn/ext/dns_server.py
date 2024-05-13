40c381cf802d0a5cce9e3477a6555dbcf4806259
lakton <lakton1337@gmail.com>
2024-05-13 12:24:38 +0300

    фыв

--- application/sdn/ext/dns_server.py
+++ application/sdn/ext/dns_server.py
@@ -1,44 +1,23 @@
 from scapy.all import *
 
-DNSServerIP = "100.0.0.25"
-filter = "udp port 53 and ip dst " + DNSServerIP + " and not ip src " + DNSServerIP
+# IP адреса DNS серверов
+dns_servers = ["100.0.0.20", "100.0.0.21", "100.0.0.22"]
 
-def DNS_Responder(localIP):
- 
-    def getResponse(pkt):
- 
-        if DNS in pkt and pkt[DNS].opcode == 0 and pkt[DNS].ancount == 0 and pkt[IP].src != localIP:
-            if b"sdnithub1.ru" in pkt[DNSQR].qname:
-                spfResp = IP(dst=pkt[IP].src)\
-                    /UDP(dport=pkt[UDP].sport, sport=53)\
-                    /DNS(id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname,rdata="100.0.0.20")\
-                    /DNSRR(rrname="sdnithub1.ru",rdata="100.0.0.20"))
-                send(spfResp, verbose=0)
-                return "Spoofed DNS Response Sent"
-                
-            if b"sdnithub2.ru" in pkt[DNSQR].qname:
-                spfResp = IP(dst=pkt[IP].src)\
-                    /UDP(dport=pkt[UDP].sport, sport=53)\
-                    /DNS(id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname,rdata="100.0.0.21")\
-                    /DNSRR(rrname="sdnithub2.ru",rdata="100.0.0.21"))
-                send(spfResp, verbose=0)
-                return "Spoofed DNS Response Sent"
+# Фильтр для перехвата DNS запросов к виртуальному IP адресу
+filter = "udp port 53 and ip dst 100.0.0.25"
 
-            if b"sdnithub3.ru" in pkt[DNSQR].qname:
-                spfResp = IP(dst=pkt[IP].src)\
-                    /UDP(dport=pkt[UDP].sport, sport=53)\
-                    /DNS(id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname,rdata="100.0.0.22")\
-                    /DNSRR(rrname="sdnithub3.ru",rdata="100.0.0.22"))
-                send(spfResp, verbose=0)
-                return "Spoofed DNS Response Sent"
+def balanceDNS(pkt):
+    # Проверяем, что пакет содержит DNS запрос и не является ответом на запрос
+    if DNS in pkt and pkt[DNS].qr == 0:
+        # Извлекаем имя запрошенного домена
+        domain_name = pkt[DNSQR].qname.decode('utf-8')
+        # Вычисляем хэш от имени домена
+        domain_hash = hash(domain_name)
+        # Вычисляем индекс сервера в кластере на основе хэша
+        server_index = domain_hash % len(dns_servers)
+        # Отправляем ответ на запрос на выбранный сервер
+        response = IP(dst=dns_servers[server_index])/UDP(sport=53)/DNS(id=pkt[DNS].id, an=DNSRR(rrname=domain_name, rdata=dns_servers[server_index]))
+        send(response, verbose=0)
 
-            else:
-                #make DNS query, capturing the answer and send the answer
-                return forwardDNS(pkt)
- 
-        else:
-            return False
- 
-    return getResponse
- 
-sniff(filter=filter, prn=DNS_Responder(DNSServerIP))
+# Запускаем перехват пакетов и балансировку нагрузки
+sniff(filter=filter, prn=balanceDNS)
