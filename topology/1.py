488f8ee3521b005a8ef2ab821dae0d7afee50897
lakton <lakton1337@gmail.com>
2024-05-17 13:58:10 +0300

    ФЫВ

--- topology/1.py
+++ topology/1.py
@@ -13,43 +13,45 @@ time.sleep(3)  # Задержка в 3 секунды
 # Отправим ICMP Echo Reply в ответ на ICMP Echo Request
 icmp_reply = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/ICMP(type=0)
 send_fake_response(icmp_reply, "h1-eth0")
-time.sleep(3)  # Задержка в 3 секунды
 
 # HTTP POST: Создадим HTTP POST запрос и отправим его
 http_post_req = Ether()/IP(src="100.0.0.10", dst="100.0.0.45")/TCP(dport=80)/("POST / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Length: 9\r\n\r\nuser=foo")
 sendp(http_post_req, iface="h1-eth0")
-time.sleep(3)  # Задержка в 3 секунды
 
 # Отправим поддельный HTTP Response (200 OK) для успешной обработки HTTP POST запроса
-http_post_response = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/TCP(sport=80)/("HTTP/1.1 200 OK\r\n\r\nSuccessful POST request processed")
-send_fake_response(http_post_response, "h1-eth0")
+http_post_response = (
+    Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/
+    TCP(sport=80)/
+    ("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\nSuccessful POST request processed")
+)
 time.sleep(3)  # Задержка в 3 секунды
 
-# HTTP PUT: Создадим HTTP PUT запрос и отправим его
-http_put_req = Ether()/IP(src="100.0.0.10", dst="100.0.0.45")/TCP(dport=80)/("PUT / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Length: 10\r\n\r\nHelloWorld")
-sendp(http_put_req, iface="h1-eth0")
+# HTTP PUT (INSERT): Создадим HTTP PUT запрос для операции INSERT и отправим его
+http_put_insert_req = Ether()/IP(src="100.0.0.10", dst="100.0.0.45")/TCP(dport=80)/("PUT / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Length: 10\r\n\r\ncat /var/log/")
+sendp(http_put_insert_req, iface="h1-eth0")
+
+
+# Отправим поддельный HTTP Response (403 Forbidden) для блокировки запроса PUT (INSERT)
+http_put_insert_response = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/TCP(sport=80)/("HTTP/1.1 403 Forbidden\r\n\r\nPUT (INSERT) request blocked")
+send_fake_response(http_put_insert_response, "h1-eth0")
 time.sleep(3)  # Задержка в 3 секунды
 
-# Отправим поддельный HTTP Response (200 OK) для успешного приема HTTP PUT запроса
-http_put_response = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/TCP(sport=80)/("HTTP/1.1 200 OK\r\n\r\nSuccessful PUT request processed")
-send_fake_response(http_put_response, "h1-eth0")
+# HTTP PUT (UPDATE): Создадим HTTP PUT запрос для операции UPDATE и отправим его
+http_put_update_req = Ether()/IP(src="100.0.0.10", dst="100.0.0.45")/TCP(dport=80)/("PUT / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Length: 10\r\n\r\ncat /var/log/")
+sendp(http_put_update_req, iface="h1-eth0")
+
+
+# Отправим поддельный HTTP Response (403 Forbidden) для блокировки запроса PUT (UPDATE)
+http_put_update_response = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/TCP(sport=80)/("HTTP/1.1 403 Forbidden\r\n\r\nPUT (UPDATE) request blocked")
+send_fake_response(http_put_update_response, "h1-eth0")
 time.sleep(3)  # Задержка в 3 секунды
 
-# HTTP PUT injection: Создадим поддельный HTTP PUT запрос с вредоносным содержимым и отправим его
-http_put_malicious_req = Ether()/IP(src="100.0.0.10", dst="100.0.0.45")/TCP(dport=80)/("PUT / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Length: 15\r\n\r\ncat /etc/passwd")
-sendp(http_put_malicious_req, iface="h1-eth0")
-time.sleep(3)  # Задержка в 3 секунды
+# HTTP PUT (DELETE): Создадим HTTP PUT запрос для операции DELETE и отправим его
+http_put_delete_req = Ether()/IP(src="100.0.0.10", dst="100.0.0.45")/TCP(dport=80)/("PUT / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Length: 10\r\n\r\ncat /var/log/")
+sendp(http_put_delete_req, iface="h1-eth0")
 
-# Отправим поддельный HTTP Response (403 Forbidden) для блокировки вредоносного HTTP PUT запроса
-http_put_injection_response = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/TCP(sport=80)/("HTTP/1.1 403 Forbidden\r\n\r\nMalicious PUT request blocked")
-send_fake_response(http_put_injection_response, "h1-eth0")
-time.sleep(3)  # Задержка в 3 секунды
 
-# HTTP GET: Создадим HTTP GET запрос с вредоносным содержимым и отправим его
-http_get_malicious_req = Ether()/IP(src="100.0.0.10", dst="100.0.0.45")/TCP(dport=80)/("GET / HTTP/1.1\r\nHost: 100.0.0.45\r\n\r\nwget -O -")
-sendp(http_get_malicious_req, iface="h1-eth0")
+# Отправим поддельный HTTP Response (403 Forbidden) для блокировки запроса PUT (DELETE)
+http_put_delete_response = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/TCP(sport=80)/("HTTP/1.1 403 Forbidden\r\n\r\nPUT (DELETE) request blocked")
+send_fake_response(http_put_delete_response, "h1-eth0")
 time.sleep(3)  # Задержка в 3 секунды
-
-# Отправим поддельный HTTP Response (403 Forbidden) для блокировки вредоносного HTTP GET запроса
-http_get_response = Ether()/IP(src="100.0.0.45", dst="100.0.0.10")/TCP(sport=80)/("HTTP/1.1 403 Forbidden\r\n\r\nMalicious GET request blocked")
-send_fake_response(http_get_response, "h1-eth0")
