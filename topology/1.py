848e2784973696ff47da0888f3831814e0afc590
lakton <lakton1337@gmail.com>
2024-05-21 10:38:10 +0300

    asd

--- topology/1.py
+++ topology/1.py
@@ -1,34 +1,8 @@
 from scapy.all import *
 
-# Определение целевого хоста и порта для TCP handshake
-target_ip = "100.0.0.45"  # Замените на реальный IP-адрес целевого сервера
-target_port = 80  # Замените на реальный порт целевого сервера
+# Создание HTTP POST-запроса
+http_request = Ether(src="00:00:00:00:00:04", dst="fe:91:b3:92:f1:98") / IP(src="100.0.0.10", dst="100.0.0.45") / TCP(dport=80, sport=20) / \
+                "POST / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Type: application/json\r\nContent-Length: 18\r\n\r\n{\"data\": \"example\"}"
 
-# Исходные IP и интерфейс отправителя
-source_ip = "100.0.0.10"
-source_interface = "h1-eth0"
-
-# Симулируем TCP handshake (отправляем SYN)
-syn_packet = IP(src=source_ip, dst=target_ip) / TCP(sport=12345, dport=target_port, flags="S")
-
-# Отправляем SYN пакет и получаем SYN/ACK
-syn_ack_response = sr1(syn_packet, iface=source_interface)
-
-if syn_ack_response:
-    print("TCP Handshake: SYN sent and SYN/ACK received successfully")
-    print("Response:", syn_ack_response.show())
-else:
-    print("No SYN/ACK response received")
-
-# HTTP POST запрос
-http_post_request = IP(src=source_ip, dst=target_ip) / TCP(sport=syn_ack_response[TCP].dport, dport=target_port, flags="PA") / \
-                    "POST /submit HTTP/1.1\r\nHost: example.com\r\nContent-Length: 7\r\n\r\nmessage"
-
-# Отправляем HTTP POST запрос и получаем ответ
-http_post_response = sr1(http_post_request, iface=source_interface)
-
-if http_post_response:
-    print("HTTP POST request sent successfully")
-    print("Response:", http_post_response.show())
-else:
-    print("No response received")
+# Отправка запроса
+sendp(http_request, iface="h1-eth0")
