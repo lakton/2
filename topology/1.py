1c03f949c182943a261bbf841cb97e85faed7f76
lakton <lakton1337@gmail.com>
2024-05-21 10:35:51 +0300

    asd

--- topology/1.py
+++ topology/1.py
@@ -8,21 +8,27 @@ target_port = 80  # Замените на реальный порт целево
 source_ip = "100.0.0.10"
 source_interface = "h1-eth0"
 
+# Симулируем TCP handshake (отправляем SYN)
+syn_packet = IP(src=source_ip, dst=target_ip) / TCP(sport=12345, dport=target_port, flags="S")
+
+# Отправляем SYN пакет и получаем SYN/ACK
+syn_ack_response = sr1(syn_packet, iface=source_interface)
+
+if syn_ack_response:
+    print("TCP Handshake: SYN sent and SYN/ACK received successfully")
+    print("Response:", syn_ack_response.show())
+else:
+    print("No SYN/ACK response received")
+
 # HTTP POST запрос
 http_post_request = IP(src=source_ip, dst=target_ip) / TCP(sport=syn_ack_response[TCP].dport, dport=target_port, flags="PA") / \
                     "POST /submit HTTP/1.1\r\nHost: example.com\r\nContent-Length: 7\r\n\r\nmessage"
 
-# Симулируем ответ на HTTP POST запрос
-http_response = IP(src=target_ip, dst=source_ip) / TCP(sport=target_port, dport=syn_ack_response[TCP].dport, flags="PA") / \
-                "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 13\r\n\r\nHello, World!"
-
-# Отправляем запрос и получаем ответ
+# Отправляем HTTP POST запрос и получаем ответ
 http_post_response = sr1(http_post_request, iface=source_interface)
+
 if http_post_response:
     print("HTTP POST request sent successfully")
     print("Response:", http_post_response.show())
 else:
     print("No response received")
-
-# Отправляем HTTP ответ
-send(http_response, iface=source_interface)
