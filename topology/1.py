36903f643e6042e2c05b715eabe56211629a00be
lakton <lakton1337@gmail.com>
2024-05-21 10:32:03 +0300

    asd

--- topology/1.py
+++ topology/1.py
@@ -21,8 +21,20 @@ ack_packet = IP(src=source_ip, dst=target_ip) / TCP(sport=syn_ack_response[TCP].
                                                     flags="A", ack=syn_ack_response[TCP].seq + 1, seq=syn_ack_response[TCP].ack)
 send(ack_packet, iface=source_interface)
 
-# Ответ на HTTP запрос
+# HTTP POST запрос
+http_post_request = IP(src=source_ip, dst=target_ip) / TCP(sport=syn_ack_response[TCP].dport, dport=target_port, flags="PA") / \
+                    "POST /submit HTTP/1.1\r\nHost: example.com\r\nContent-Length: 7\r\n\r\nmessage"
+
 http_response = IP(src=target_ip, dst=source_ip) / TCP(sport=target_port, dport=syn_ack_response[TCP].dport, flags="PA") / \
                 "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 13\r\n\r\nHello, World!"
 
-send(http_response)
+# Отправляем запрос и получаем ответ
+http_post_response = sr1(http_post_request, iface=source_interface)
+if http_post_response:
+    print("HTTP POST request sent successfully")
+    print("Response:", http_post_response.show())
+else:
+    print("No response received")
+
+# Отправляем HTTP ответ
+send(http_response, iface=source_interface)
