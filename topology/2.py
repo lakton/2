de64e28c30fcef0805e9d3c0f23e7dc011c97c26
lakton <lakton1337@gmail.com>
2024-05-21 10:45:46 +0300

    asd

--- topology/2.py
+++ topology/2.py
@@ -1,6 +1,12 @@
 from scapy.all import *
-
-# Создание HTTP POST-ответа
+import time
+# Создание HTTP POST-запроса
+http_request = Ether(src="00:00:00:00:00:04", dst="fe:91:b3:92:f1:98") / IP(src="100.0.0.10", dst="100.0.0.45") / TCP(dport=80, sport=20) / \
+                "POST / HTTP/1.1\r\nHost: 100.0.0.45\r\nContent-Type: application/json\r\nContent-Length: 18\r\n\r\n{\"data\": \"example\"}"
+# Отправка ответа
+sendp(http_request, iface="h1-eth0")
+time.sleep(5)
+# Симуляция ответа на HTTP POST-запрос
 http_response = Ether(src="fe:91:b3:92:f1:98", dst="00:00:00:00:00:04") / IP(src="100.0.0.45", dst="100.0.0.10") / TCP(dport=20, sport=80) / \
                 "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!"
 
