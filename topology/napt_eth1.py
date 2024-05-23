41fe11a18f7c2abfd3218c8031417ff244cb918d
lakton <lakton1337@gmail.com>
2024-05-23 10:43:58 +0300

    asd

--- topology/napt_eth1.py
+++ topology/napt_eth1.py
@@ -1,12 +1,10 @@
 from scapy.all import IP, ICMP, TCP, UDP, send
 
 # Отправка различных типов пакетов
-icmp_packet = IP(src="100.0.0.45", dst="100.0.0.1")/ICMP()
-tcp_packet = IP(src="100.0.0.45", dst="100.0.0.1")/TCP(dport=80)
-udp_packet = IP(src="100.0.0.25", dst="100.0.0.1")/UDP(dport=53)
 icmp_packet1 = IP(src="100.0.0.1", dst="10.0.0.1")/ICMP()
 tcp_packet1 = IP(src="100.0.0.1", dst="10.0.0.1")/TCP(dport=80)
 udp_packet1 = IP(src="100.0.0.1", dst="10.0.0.1")/UDP(dport=53)
+
 icmp_packet2 = IP(src="10.0.0.1", dst="10.0.0.50")/ICMP()
 tcp_packet2 = IP(src="10.0.0.1", dst="10.0.0.50")/TCP(dport=80)
 udp_packet2 = IP(src="10.0.0.1", dst="10.0.0.50")/UDP(dport=53)
