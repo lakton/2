1b0a3820c6bf4167a877a0ef0e4644072396a177
lakton <lakton1337@gmail.com>
2024-05-23 10:26:31 +0300

    фыв

--- topology/napt_fil.py
+++ topology/napt_fil.py
@@ -6,19 +6,7 @@ tcp_packet = IP(dst="100.0.0.45")/TCP(dport=80)
 udp_packet = IP(dst="100.0.0.25")/UDP(dport=53)
 malicious_tcp_packet = IP(dst="100.0.0.11")/TCP(dport=10851) # Малicious TCP пакет
 
-send(icmp_packet)
-send(tcp_packet)
-send(udp_packet)
-send(malicious_tcp_packet)
-
-# Функция для обработки захваченных пакетов
-def packet_callback(packet):
-    if packet.haslayer(ICMP):
-        print(f"Получен ICMP пакет от {packet[IP].src} к {packet[IP].dst}")
-    elif packet.haslayer(TCP):
-        print(f"Получен TCP пакет от {packet[IP].src} к {packet[IP].dst} на порт {packet[TCP].dport}")
-    elif packet.haslayer(UDP):
-        print(f"Получен UDP пакет от {packet[IP].src} к {packet[IP].dst} на порт {packet[UDP].dport}")
-
-# Захват пакетов
-sniff(filter="ip", prn=packet_callback, count=10)
+send(icmp_packet, iface="napt-eth2")
+send(tcp_packet, iface="napt-eth2")
+send(udp_packet, iface="napt-eth2")
+send(malicious_tcp_packet, iface="napt-eth2")
