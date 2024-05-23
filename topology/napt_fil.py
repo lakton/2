9c37b84bfe65eb94a9d8755039b1c6fa0fd63fcb
lakton <lakton1337@gmail.com>
2024-05-23 10:29:39 +0300

    фыв

--- topology/napt_fil.py
+++ topology/napt_fil.py
@@ -1,7 +1,7 @@
 from scapy.all import IP, ICMP, TCP, UDP, send, sniff
 
 # Отправка различных типов пакетов
-icmp_packet = IP(dst="10.0.0.1")/ICMP()
+icmp_packet = IP(dst="100.0.0.25")/ICMP()
 tcp_packet = IP(dst="100.0.0.45")/TCP(dport=80)
 udp_packet = IP(dst="100.0.0.25")/UDP(dport=53)
 malicious_tcp_packet = IP(dst="100.0.0.11")/TCP(dport=10851) # Малicious TCP пакет
