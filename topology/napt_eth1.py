43406b68a4ad9d700e919e11d2e6dc5ef6e00420
lakton <lakton1337@gmail.com>
2024-05-23 10:44:07 +0300

    asd

--- topology/napt_eth1.py
+++ topology/napt_eth1.py
@@ -9,11 +9,6 @@ icmp_packet2 = IP(src="10.0.0.1", dst="10.0.0.50")/ICMP()
 tcp_packet2 = IP(src="10.0.0.1", dst="10.0.0.50")/TCP(dport=80)
 udp_packet2 = IP(src="10.0.0.1", dst="10.0.0.50")/UDP(dport=53)
 
-# Отправка пакетов через интерфейс napt-eth1
-send(icmp_packet)
-send(tcp_packet)
-send(udp_packet)
-
 send(icmp_packet1)
 send(tcp_packet1)
 send(udp_packet1)
